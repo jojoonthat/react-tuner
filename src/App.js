90690f4f56f23db94d2b8d70828d5443cc998954
Joanne Pan <jpan0917@gmail.com>
2021-06-02 01:13:53 -0700

    convert stream to frequency

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import Tuner from './Tuner';
 
 function App() {
+  const [hasMicPerm, setHasMicPerm] = useState(false)
+  const [currentFrequency, setCurrentFrequency] = useState("")
+
+  useEffect(() => {
+    if (!hasMicPerm) {
+      navigator.mediaDevices.getUserMedia({ "audio": true })
+        .then(rawStream => {
+          console.log('Got mic permission!')
+          setHasMicPerm(true)
+          processStream(rawStream)
+        })
+        .catch(err => {
+          // hasMicPerm should stay false
+          console.log(`Does not have mic permission: ${err.name}`)
+        });
+    }
+  });
+
+  const processStream = async rawStream => {
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    const analyser = audioCtx.createAnalyser();
+    analyser.fftSize = 32768;
+    const stream = audioCtx.createMediaStreamSource(rawStream);
+    stream.connect(analyser);
+    const nyquist = audioCtx.sampleRate / 2;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Float32Array(bufferLength);
+    while (true) {
+      analyser.getFloatFrequencyData(dataArray);
+      let loudest = -Infinity;
+      let loudestIndex = -1;
+      for (let i = 0; i < bufferLength; i++) {
+        let currentLoudness = dataArray[i];
+        if (currentLoudness > loudest) {
+          loudest = currentLoudness;
+          loudestIndex = i;
+        }
+      }
+      const frequency = loudestIndex * (nyquist / bufferLength)
+      setCurrentFrequency(frequency)
+      await sleep(50);
+    }
+  }
+
+  const sleep = ms => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Conditional jsx to display different things based on the value of hasMicPerm */}
+      {/* i.e. if hasMicPerm, show this, else show that */}
+      <Tuner frequency={currentFrequency} />
     </div>
   );
 }
---
+++ src/Tuner.js
@@ -0,0 +1,9 @@
+function Tuner(props) {
+  return (
+    <div className="Tuner">
+        <h1>{props.frequency}</h1>
+    </div>
+  );
+}
+
+export default Tuner;
